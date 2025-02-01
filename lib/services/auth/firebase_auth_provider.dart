be3410603ec8e72aa49764a293ad6dfcb059461b
kazi ikram <kazi.ikram678@gmail.com>
2025-02-01 12:15:58 +0600

    step-9

--- lib/services/auth/firebase_auth_provider.dart
+++ lib/services/auth/firebase_auth_provider.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/services/auth/auth_user.dart';
 import 'package:mynotes/services/auth/auth_provider.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
@@ -39,11 +41,9 @@ class FirebaseAuthProvider implements AuthProvider {
   @override
   AuthUser? get currentUser {
     final user = FirebaseAuth.instance.currentUser;
-    if(user!=null)
-    {
+    if (user != null) {
       return AuthUser.fromFirebase(user);
-    }
-    else {
+    } else {
       return null;
     }
   }
@@ -52,10 +52,10 @@ class FirebaseAuthProvider implements AuthProvider {
   Future<AuthUser?> logIn({
     required String email,
     required String password,
-  }) async{
+  }) async {
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email, 
+        email: email,
         password: password,
       );
       final user = currentUser;
@@ -64,44 +64,43 @@ class FirebaseAuthProvider implements AuthProvider {
       } else {
         throw UserNotFoundAuthException();
       }
-    } on FirebaseAuthException catch(e) {
+    } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         throw UserNotFoundAuthException();
-                } else if (e.code == 'invalid-credential') {
-                  throw InvalidEmailAuthException();
-                } else {
-                  throw GenericAuthException();
-                }
-              } catch (e) {
-                throw GenericAuthException();
-              }
+      } else if (e.code == 'invalid-credential') {
+        throw InvalidEmailAuthException();
+      } else {
+        throw GenericAuthException();
+      }
+    } catch (e) {
+      throw GenericAuthException();
     }
-    
-@override
-Future<void> logOut() async{
-  final user = FirebaseAuth.instance.currentUser;
-  if(user!=null)
-  {
-    await FirebaseAuth.instance.signOut();
-
   }
-  else
-  {
-    throw  UserNotFoundAuthException();
-  }
-}
-
 
   @override
-  Future<void> sendEmailVerification() async{
+  Future<void> logOut() async {
     final user = FirebaseAuth.instance.currentUser;
-    if(user!=null)
-    {
-      await user.sendEmailVerification();
-    }
-    else {
+    if (user != null) {
+      await FirebaseAuth.instance.signOut();
+    } else {
       throw UserNotFoundAuthException();
     }
   }
 
+  @override
+  Future<void> sendEmailVerification() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      await user.sendEmailVerification();
+    } else {
+      throw UserNotFoundAuthException();
+    }
+  }
+
+  @override
+  Future<void> initialize() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
 }
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_exceptions.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -59,12 +60,11 @@ class _LoginViewState extends State<LoginView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                await FirebaseAuth.instance.signInWithEmailAndPassword(
-                  email: email,
-                  password: password,
-                );
-                final user = FirebaseAuth.instance.currentUser;
-                if (user?.emailVerified ?? false) {
+                await AuthService.firebase()
+                    .logIn(email: email, password: password);
+
+                final user = AuthService.firebase().currentUser;
+                if (user?.isEmailVerified ?? false) {
                   Navigator.of(context).pushNamedAndRemoveUntil(
                     notesRoute,
                     (route) => false,
@@ -75,17 +75,12 @@ class _LoginViewState extends State<LoginView> {
                     (route) => false,
                   );
                 }
-              } on FirebaseAuthException catch (e) {
-                if (e.code == 'user-not-found') {
-                  await showErrorDialog(context, 'User-Not-Found');
-                } else if (e.code == 'invalid-credential') {
-                  await showErrorDialog(
-                      context, 'invalid User_name or Password');
-                } else {
-                  await showErrorDialog(context, 'Error: ${e.code}');
-                }
-              } catch (e) {
-                await showErrorDialog(context, e.toString());
+              } on UserNotFoundAuthException {
+                await showErrorDialog(context, 'User-Not-Found');
+              } on InvalidEmailAuthException {
+                await showErrorDialog(context, 'invalid User_name or Password');
+              } on GenericAuthException {
+                await showErrorDialog(context, 'Authentication Error');
               }
             },
             child: Text('Login'),
